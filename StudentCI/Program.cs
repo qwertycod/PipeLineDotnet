f5b59d2fca0f6f79e7f2a60ec8aa26f71a975ade
Qwertycod <qwertycodd@gmail.com>
2023-09-07 13:33:47 +0530

    update

--- StudentCI/Program.cs
+++ StudentCI/Program.cs
@@ -2,18 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using StudentCI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddDbContext<SchoolContext>(options =>
    options.UseNpgsql(builder.Configuration.GetConnectionString("SchoolContext")));
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -34,17 +28,14 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-
-}
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
 
