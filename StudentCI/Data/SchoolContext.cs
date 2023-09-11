951406acedb2fcbd73bc8ce6b22237adb11da1e9
Qwertycod <qwertycodd@gmail.com>
2023-09-11 19:13:06 +0530

    removing student controller

--- StudentCI/Data/SchoolContext.cs
+++ StudentCI/Data/SchoolContext.cs
@@ -6,7 +6,6 @@ namespace StudentCI.Data
     public class SchoolContext : DbContext
     {
         public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
-        public DbSet<Models.Student>? students { get; set; }
         public DbSet<Models.Product>? products { get; set; }
         public DbSet<Models.Bird>? birds { get; set; }
     }
--- StudentCI/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentCI.Models
-{
-    public class Student
-    {
-        public int id { get; set; }
-        public string? lastname { get; set; }
-        public string? firstmidname { get; set; }
-    }
-}
